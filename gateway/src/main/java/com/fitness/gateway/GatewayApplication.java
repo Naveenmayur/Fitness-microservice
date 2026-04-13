8b57808672de20a39b293c1b5b7d9844b6f6e29a
Naveenmayur <naveenmayurkr@gmail.com>
2026-04-13 16:21:03 +0530

    feat: add api gateway and register config server as discovery client
    - Initialize gateway microservice with Spring Cloud Gateway\n- Configure routing for activity-service, ai-service, and user-service\n- Register configserver as a Eureka client using @EnableDiscoveryClient\n- Update user-service configuration in Config Server to include server.port: 8081

--- gateway/src/main/java/com/fitness/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/fitness/gateway/GatewayApplication.java
@@ -1,17 +1,15 @@
-package com.fitness.configserver;
+package com.fitness.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableDiscoveryClient
-public class ConfigserverApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigserverApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
