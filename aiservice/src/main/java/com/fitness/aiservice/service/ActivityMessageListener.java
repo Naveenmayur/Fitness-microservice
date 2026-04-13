0a3463fce9df908cf3f226ccf7093f30f9fefde1
Naveenmayur <naveenmayurkr@gmail.com>
2026-04-13 15:08:57 +0530

    feat: add config server and persist recommendations in aiservice
    - Initialize configserver microservice\n- Add centralized configuration for activity-service, ai-service, and user-service\n- Update ActivityMessageListener to save generated recommendations to MongoDB

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -3,6 +3,8 @@ package com.fitness.aiservice.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -15,10 +17,13 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "${rabbitmq.queue.name}")
     public void processActivity(Activity activity) throws JsonProcessingException {
         log.info("Received activity message: {}", activity.getId());
-        log.info("Generated Recommendation: {}", activityAIService.generateRecommendation(activity));
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
+
+        recommendationRepository.save(recommendation);
     }
 }
